23455e37279641c8fd0741ad62bfec492b879b5d
podhmo <ababjam61+github@gmail.com>
2022-11-12 20:03:04 +0900

    chore: add examples

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 check:
-	go run . -debug _examples/foo/foo.go
+	mkdir -p _examples/foo_transformed
+	go run . -debug _examples/foo/foo.go > _examples/foo_transformed/foo.go
+	diff -u _examples/foo/foo.go  _examples/foo_transformed/foo.go | tee _examples/foo.diff
+	
---
+++ _examples/foo_transformed/foo.go
@@ -0,0 +1,36 @@
+package internal
+
+import (
+	"fmt"
+)
+
+func Foo() error {
+	if err := Bar(); err != nil {
+		return fmt.Errorf("foo -- %w", err)
+	}
+	return nil
+}
+
+func Bar() error {
+	if err := Boo(); err != nil {
+		return fmt.Errorf("on %s%d -- %w", "bar", 0, err)
+	}
+	return nil
+}
+func Boo() error {
+	return fmt.Errorf("hmm")
+}
+
+func Multiples() (bool, error) {
+	if err := Bar(); err != nil {
+		return false, fmt.Errorf("multiples -- %w", err)
+	}
+	return true, nil
+}
+
+func NeverChanges() error {
+	if err := Bar(); err != nil {
+		return fmt.Errorf("foo -- %w", err)
+	}
+	return nil
+}
